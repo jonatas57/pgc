7e8dfb0f18cf29fc7f105d7e9217782c8b9017a7
jonatas57 <jdjonatasduarte57@gmail.com>
2025-06-29 22:25:43 -0300

    algoritmo de pattern matching - CSES Finding Patterns

---
+++ algoritmos/stringMatching/arvore.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long      ll;
+typedef vector<int>    vi;
+typedef vector<bool>   vb;
+typedef pair<int, int> ii;
+
+const int INF = 0x3f3f3f3f;
+const ll  INFLL = 0x3f3f3f3f3f3f3f3fll;
+
+#define each(x, s)  for (auto& x : s)
+#define loop(x)     for (int i = 0;i < x;i++)
+#define vloop(v, x) for (int v = 0;v < x;v++)
+#define iter(a)     a.begin(), a.end()
+#define riter(a)    a.rbegin(), a.rend()
+#define endl        "\n"
+
+struct SuffixTree {
+  struct node {
+    int l, r, par, link;
+    map<char, int> next;
+
+    node(int l = 0, int r = 0, int par = -1) : l(l), r(r), par(par), link(-1) {}
+    int len() { return r - l; }
+    int& operator[](char c) {
+      if (!next.count(c)) next[c] = -1;
+      return next[c];
+    }
+  };
+
+  struct state {
+    int v, pos;
+    state(int v = 0, int pos = 0) : v(v), pos(pos) {}
+  };
+
+  state ptr;
+  vector<node> t;
+  string str;
+  int sz;
+
+  SuffixTree(string s) : t(1), str(s), sz(1) {
+    for (int i = 0;s[i];i++) {
+      extend(i);
+    }
+  }
+
+  void extend(int pos) {
+    while (true) {
+      state nptr = go(ptr, pos, pos + 1);
+      if (nptr.v != -1) {
+        ptr = nptr;
+        return;
+      }
+
+      int mid = split(ptr);
+      int leaf = sz++;
+      t.emplace_back(pos, str.size(), mid);
+      t[mid][str[pos]] = leaf;
+      ptr.v = getLink(mid);
+      ptr.pos = t[ptr.v].len();
+      if (!mid) break;
+    }
+  }
+
+  state go(state st, int l, int r) {
+    while (l < r) {
+      if (st.pos == t[st.v].len()) {
+        st = state(t[st.v][str[l]], 0);
+        if (st.v == -1) return st;
+      }
+      else {
+        if (str[t[st.v].l + st.pos] != str[l]) return state(-1, -1);
+        if (r - l < t[st.v].len() - st.pos) return state(st.v, st.pos + r - l);
+        l += t[st.v].len() - st.pos;
+        st.pos = t[st.v].len();
+      }
+    }
+    return st;
+  }
+
+  int split(state st) {
+    if (st.pos == t[st.v].len()) {
+      return st.v;
+    }
+    if (st.pos == 0) {
+      return t[st.v].par;
+    }
+    node v = t[st.v];
+    int id = sz++;
+    t.emplace_back(v.l, v.l + st.pos, v.par);
+    t[v.par][str[v.l]] = id;
+    t[id][str[v.l + st.pos]] = st.v;
+    t[st.v].par = id;
+    t[st.v].l += st.pos;
+    return id;
+  }
+
+  int getLink(int v) {
+    if (t[v].link != -1) return t[v].link;
+    if (t[v].par == -1) return 0;
+    int to = getLink(t[v].par);
+    return t[v].link = split(go(state(to, t[to].len()), t[v].l + (t[v].par == 0), t[v].r));
+  }
+
+  node& operator[](int v) {
+    return t[v];
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+  
+  string s;
+  int k;
+  cin >> s >> k;
+  SuffixTree st(s);
+  loop(k) {
+    string t;
+    cin >> t;
+    int v = 0, pos = 0;
+    bool ok = true;
+    each(c, t) {
+      if (!ok) break;
+      if (pos == st[v].len()) {
+        v = st[v][c];
+        pos = 1;
+      }
+      else if (c == st.str[st[v].l + pos]) {
+        pos++;
+      }
+      else v = -1;
+      ok = v != -1;
+    }
+    cout << (ok ? "YES" : "NO") << endl;
+  }
+  return 0;
+}
+
